ac624e608bbadeef0de5e80b055a739a26029961
Bruno Padilha <brpadilha.dev@gmail.com>
2020-08-02 23:46:20 -0300

    first commit

---
+++ app/models/airline.rb
@@ -0,0 +1,17 @@
+class Airline < ApplicationRecord
+  has_many :reviews
+
+  #antes de criar no banco de dados, chamar essa função que ja cria o slug com o nome que iremos passar
+  before_create :slugify
+
+  #quando criar o nome da linha, ela já faz um slug
+  #United Airlines ==> united-airlines
+  def slugify
+    self.slug = name.parameterize    
+  end
+
+  #Faz uma média de todas as pontuações recebidas
+  def average_score
+    reviews.average(:score).round(2).to_f
+  end
+end
